1004669516d223bb2af32a44f177bd9b5d7514fe
Anubhav Sharma <anubhav@Anubhavs-MacBook-Air.local>
2026-02-13 13:13:47 +0530

    ok

--- app/page.jsx
+++ app/page.jsx
@@ -32,6 +32,31 @@ export default function Home() {
           gtag('config', 'AW-17909613244');
         `}
       </Script>
+
+{/* meta */}
+{/* Meta Pixel */}
+      <Script id="meta-pixel" strategy="afterInteractive">
+        {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;
+          n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];
+          t=b.createElement(e);t.async=!0;
+          t.src=v;
+          s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}
+          (window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+
+          fbq('init', '909795298216798');
+          fbq('track', 'PageView');
+        `}
+      </Script>
+
+
+
       <Header1 />
       <Hero />
       <div className="main-content ">
